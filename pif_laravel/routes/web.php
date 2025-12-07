57eb9c17a4803f51d97939fcf22d970a48c49551
Alfian Ramadani <alfianr792@gmail.com>
2025-12-07 20:18:15 +0800

    fix docker1

--- pif_laravel/routes/web.php
+++ pif_laravel/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\PerformanceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/test-performance', [PerformanceController::class, 'testPerformance']);
